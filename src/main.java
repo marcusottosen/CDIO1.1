e34bfae1a9109101edef9ee3a069ad92d658fa62
marcusottosen <mkottosen@gmail.com>
2020-09-25 14:34:45 +0200

    Starting comments

--- src/main.java
+++ src/main.java
@@ -1,4 +1,23 @@
 public class main {
+    public static void main(String[] args) {
 
 
+        //----------------------------------------
+        //OPRETTELSE AF DICE & SPILLER 1 & 2
+        //----------------------------------------
+
+
+
+        //----------------------------------------
+        //LOGIK
+        //----------------------------------------
+
+        // Find vinder af runde
+
+        //add score til spiller
+
+        // Har en spiller 40points? find vinder
+
+
+    }
 }
