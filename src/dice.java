e493ffb7bbf520c2f39f2fe68bfe8c6ebea956f2
Villads <villadsandersen1@gmail.com>
2020-09-25 14:17:51 +0200

    Terning1

--- src/dice.java
+++ src/dice.java
@@ -1,2 +1,13 @@
 public class dice {
+    private int MAX = 6; //Maximum værdi
+
+    private int faceValue; //nuværende værdi
+
+    //Konstruktør
+    public dice(int value)
+    {
+        faceValue = value;
+    }
+
+
 }
