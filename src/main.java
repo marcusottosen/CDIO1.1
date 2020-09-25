16d3df0bd081a1dbb876572f41e8df077d28d01c
Victor E S Kongsbak <54668748+Kongsbak99@users.noreply.github.com>
2020-09-25 15:11:05 +0200

    Tilføjet ekstraopgaver 1. og 2. (Ikke testet)

--- src/main.java
+++ src/main.java
@@ -8,10 +8,45 @@ public class main {
         int p1Score=0;
         int p2Score=0;
 
+        boolean p1Turn, p2Turn;
+        p1Turn = true;
+        p2Turn = false;
+
         Dice d1 = new Dice();
         Dice d2 = new Dice();
 
+        while (p1Score <= 40 || p2Score <= 40){
 
+            do{
+
+
+                //hvis spilleren slår 2 ens, får spilleren en ekstra tur.
+                if(d1 == d2){
+                    //Hvis begge terninger slår 1 mister spilleren alle sine point.
+                    if(d1 == 1 && d2 == 1){
+                        p1Score = 0;
+                    }
+                    p1Turn = true;
+                } else{
+                    p1Turn = false;
+                    p2Turn = true;
+                }
+            } while(p1Turn == true);
+            do{
+
+                //hvis spilleren slår 2 ens, får spilleren en ekstra tur.
+                if(d1 == d2){
+                    //Hvis begge terninger slår 1 mister spilleren alle sine point.
+                    if(d1 == 1 && d2 == 1){
+                        p2Score = 0;
+                    }
+                    p2Turn = true;
+                } else{
+                    p2Turn = false;
+                    p1Turn = true;
+                }
+            } while (p2Turn == true);
+        }
 
 
         //----------------------------------------
