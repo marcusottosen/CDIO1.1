4ab5b8ecf1f72ac5391b4947b6c960dee3914673
marcusottosen <mkottosen@gmail.com>
2020-09-28 15:11:18 +0200

    1. fungerende version af spillet uden ekstraopgaver.

--- src/main.java
+++ src/main.java
@@ -15,27 +15,32 @@ public class main {
         dice d1 = new dice();
         dice d2 = new dice();
 
-        while (p1Score <= 40 || p2Score <= 40){
-            while(p1Turn){
-                d1.roll();
-                d2.roll();
+
+        //----------------------------------------
+        //LOGIK
+        //----------------------------------------
+
+        outer: while (p1Score <= 40 || p2Score <= 40){
+            while(p1Turn){  //Spiller 1
+                d1.roll();  //Ruller terning 1
+                d2.roll();  //Ruller terning 2
                 p1Score = p1Score+d1.getFaceValue()+d2.getFaceValue();
                 System.out.println("Player 1 har "+ p1Score + " point");
                 p1Turn=false;
                 p2Turn=true;
             }
 
-            while(p2Turn){
-                d1.roll();
-                d2.roll();
+            while(p2Turn){ //Spiller 2
+                d1.roll();  //Ruller terning 1
+                d2.roll();  //Ruller terning 2
                 p2Score = p2Score+d1.getFaceValue()+d2.getFaceValue();
                 System.out.println("Player 2 har "+ p2Score + " point");
                 if (p1Score>=40 || p2Score>=40){
-                    p2Turn=false;
-                    p1Turn=false;
-                }else
-                    p2Turn=false;
-                    p1Turn=true;
+                    break outer;    //Hopper ud af det yderste while loop navngivet "outer"
+                }else {
+                    p2Turn = false;
+                    p1Turn = true;
+                }
             }
         }
 
@@ -47,19 +52,6 @@ public class main {
         } else {
             System.out.println("Player 2 has won!");
         }
-
-
-        //----------------------------------------
-        //LOGIK
-        //----------------------------------------
-
-        // Find vinder af runde
-
-        //add score til spiller
-
-        // Har en spiller 40points? find vinder
-
-
     }
 }
 
