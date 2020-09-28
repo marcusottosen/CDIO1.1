5678a2c4f747719e9e6a625d83729deb89a8af35
Rasmus Pedersen <rasmus.jdp@hotmail.com>
2020-09-28 15:20:57 +0200

    Commit 15:20 - 28/9

--- src/main.java
+++ src/main.java
@@ -1,7 +1,6 @@
 public class main {
     public static void main(String[] args) {
 
-
         //----------------------------------------
         //OPRETTELSE AF DICE & SPILLER 1 & 2
         //----------------------------------------
